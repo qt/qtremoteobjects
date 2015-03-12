d3a7dcfcd3e92c1b6c566d6d6164876d5b8c2160
Kevin Funk <kevin.funk.ford@kdab.com>
2015-03-12 10:56:35 +0100

    CMake: Provide qt5_generate_repc macro
    Provide macro in Qt5RemoteObjectsMacros.cmake.
    There's no need for further setup, qmake automagically recognizes this
    file is here and installs it to
    $QTDIR/lib/cmake/Qt5RemoteObjects/Qt5RemoteObjectsMacros.cmake
    
    The generated Qt5RemoteObjectsConfig.cmake file then includes this file,
    so as soon as we do `find_package(Qt5RemoteObjects)`, this macro is
    available.
    
    Basic usage:
      qt5_generate_repc(LIB_SRCS interface.rep SOURCE)
      qt5_generate_repc(LIB_SRCS interface.rep REPLICA)
    
    Change-Id: I0b7f6439361422e4a1e550439832aa39eb90c1df
    Reviewed-by: Björn Breitmeyer <bjoern.breitmeyer@kdab.com>
    Reviewed-by: Volker Krause <volker.krause@kdab.com>
    Reviewed-by: Brett Stottlemyer <bstottle@ford.com>

---
+++ src/remoteobjects/Qt5RemoteObjectsMacros.cmake
@@ -0,0 +1,93 @@
+#
+# Copyright (C) 2015 Ford Motor Company
+# Contact: http://www.qt-project.org/legal
+#
+# This file is part of the QtRemoteObjects module of the Qt Toolkit.
+#
+# $QT_BEGIN_LICENSE:LGPL$
+# Commercial License Usage
+# Licensees holding valid commercial Qt licenses may use this file in
+# accordance with the commercial license agreement provided with the
+# Software or, alternatively, in accordance with the terms contained in
+# a written agreement between you and Digia.  For licensing terms and
+# conditions see http://qt.digia.com/licensing.  For further information
+# use the contact form at http://qt.digia.com/contact-us.
+#
+# GNU Lesser General Public License Usage
+# Alternatively, this file may be used under the terms of the GNU Lesser
+# General Public License version 2.1 as published by the Free Software
+# Foundation and appearing in the file LICENSE.LGPL included in the
+# packaging of this file.  Please review the following information to
+# ensure the GNU Lesser General Public License version 2.1 requirements
+# will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+#
+# In addition, as a special exception, Digia gives you certain additional
+# rights.  These rights are described in the Digia Qt LGPL Exception
+# version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+#
+# GNU General Public License Usage
+# Alternatively, this file may be used under the terms of the GNU
+# General Public License version 3.0 as published by the Free Software
+# Foundation and appearing in the file LICENSE.GPL included in the
+# packaging of this file.  Please review the following information to
+# ensure the GNU General Public License version 3.0 requirements will be
+# met: http://www.gnu.org/copyleft/gpl.html.
+#
+#
+# $QT_END_LICENSE$
+
+# Macros
+# =========
+#
+# In some cases it can be necessary or useful to invoke the Qt build tools in a
+# more-manual way. Several macros are available to add targets for such uses.
+#
+# ::
+#
+#   macro qt5_generate_repc(outfiles infile outputtype)
+#         out: outfiles
+#         in: infile outputtype
+#         create C++ code from a list of .rep files. Per-directory preprocessor
+#         definitions are also added.
+#         infile should be a replicant template file (.rep)
+#         outputtype specifies output file type, it can be one of SOURCE|REPLICA
+#         Example usage: qt5_generate_repc(LIB_SRCS interface.rep SOURCE)
+#           for generating interface_source.h and adding it to LIB_SRCS
+
+get_target_property(_moc_executable Qt5::moc LOCATION)
+set(_search_dirs "${_moc_executable}/..")
+
+find_program(Qt5RemoteObjects_REPC_EXECUTABLE repc PATHS ${_search_dirs})
+if(NOT Qt5RemoteObjects_REPC_EXECUTABLE)
+    message(FATAL_ERROR "repc executable not found in dirs: ${_search_dirs} -- Check installation.")
+endif()
+
+macro(qt5_generate_repc outfiles infile outputtype)
+    # get include dirs and flags
+    get_filename_component(abs_infile ${infile} ABSOLUTE)
+    get_filename_component(infile_name "${infile}" NAME)
+    string(REPLACE ".rep" "" _infile_base ${infile_name})
+    if(${outputtype} STREQUAL "SOURCE")
+        set(_outfile_base "${_infile_base}_source")
+        set(_repc_args -o source)
+    else()
+        set(_outfile_base "${_infile_base}_replica")
+        set(_repc_args -o replica)
+    endif()
+    set(_outfile_header "${CMAKE_CURRENT_BINARY_DIR}/${_outfile_base}.h")
+    add_custom_command(OUTPUT ${_outfile_header}
+        DEPENDS ${abs_infile}
+        COMMAND ${Qt5RemoteObjects_REPC_EXECUTABLE} ${abs_infile} ${_repc_args} ${_outfile_header}
+        VERBATIM)
+    set_source_files_properties(${_outfile_header} PROPERTIES GENERATED TRUE)
+
+    qt5_get_moc_flags(_moc_flags)
+    # Make sure we get the compiler flags from the Qt5::RemoteObjects target (for includes)
+    string(REPLACE ";" ";-I" _includes "${Qt5RemoteObjects_INCLUDE_DIRS}")
+    set(_includes "-I${_includes}")
+    list(APPEND _moc_flags ${_includes})
+
+    set(_moc_outfile "${CMAKE_CURRENT_BINARY_DIR}/moc_${_outfile_base}.cpp")
+    qt5_create_moc_command(${_outfile_header} ${_moc_outfile} "${_moc_flags}" "" "")
+    list(APPEND ${outfiles} "${_outfile_header}" ${_moc_outfile})
+endmacro()
