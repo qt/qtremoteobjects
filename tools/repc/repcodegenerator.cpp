qt/qtremoteobjects

commit fc881f28987764e48237d648622e6227c2ad3237

Fix SourceApi lookup
The coverity scan pointed to a bug in SourceApi where the intent was for
the static parameter checks (based on the static QObject::connect eval)
to return the list of argument types from the lookup.  It didn't before,
now it does.

Updated repc so autogenerated code includes the same changes.

Coverity-Id: 172053
Coverity-Id: 172054
Coverity-Id: 172060
Change-Id: I8e5ef9df028d5895153cdb2a97636218f64fb618
Reviewed-by: Continuous Integration (KDAB) <build@kdab.com>
Reviewed-by: Michael Brasser <michael.brasser@live.com>

tools/repc/repcodegenerator.cpp
@@ -810,6 +810,9 @@ void RepCodeGenerator::generateSourceAPI(QTextStream &out, const ASTClass &astCl
         generateDeclarationsForEnums(out, astClass.enums, false);
     }
     out << QString::fromLatin1("    %1()").arg(className) << endl;
+    out << QStringLiteral("        : SourceApiMap()") << endl;
+    out << QStringLiteral("        , signalArgTypes{}") << endl;
+    out << QStringLiteral("        , methodArgTypes{}") << endl;
     out << QStringLiteral("    {") << endl;
     const int propCount = astClass.properties.count();
     out << QString::fromLatin1("        _properties[0] = %1;").arg(propCount) << endl;
@@ -836,12 +839,12 @@ void RepCodeGenerator::generateSourceAPI(QTextStream &out, const ASTClass &astCl
     out << QString::fromLatin1("        _signals[0] = %1;").arg(signalCount+changeSignals.size()) << endl;
     for (int i = 0; i < changedCount; ++i)
         out << QString::fromLatin1("        _signals[%1] = qtro_signal_index<ObjectType>(&ObjectType::%2, "
-                              "static_cast<void (QObject::*)()>(0),signalArgCount+%4,signalArgTypes[%4]);")
+                              "static_cast<void (QObject::*)()>(0),signalArgCount+%4,&signalArgTypes[%4]);")
                              .arg(i+1).arg(changeSignals.at(i)).arg(i) << endl;
     for (int i = 0; i < signalCount; ++i) {
         const ASTFunction &sig = astClass.signalsList.at(i);
         out << QString::fromLatin1("        _signals[%1] = qtro_signal_index<ObjectType>(&ObjectType::%2, "
-                              "static_cast<void (QObject::*)(%3)>(0),signalArgCount+%4,signalArgTypes[%4]);")
+                              "static_cast<void (QObject::*)(%3)>(0),signalArgCount+%4,&signalArgTypes[%4]);")
                              .arg(QString::number(changedCount+i+1), sig.name, sig.paramsAsString(ASTFunction::Normalized), QString::number(i)) << endl;
     }
     const int slotCount = astClass.slotsList.count();
@@ -849,7 +852,7 @@ void RepCodeGenerator::generateSourceAPI(QTextStream &out, const ASTClass &astCl
     for (int i = 0; i < slotCount; ++i) {
         const ASTFunction &slot = astClass.slotsList.at(i);
         out << QString::fromLatin1("        _methods[%1] = qtro_method_index<ObjectType>(&ObjectType::%2, "
-                              "static_cast<void (QObject::*)(%3)>(0),\"%2(%3)\",methodArgCount+%4,methodArgTypes[%4]);")
+                              "static_cast<void (QObject::*)(%3)>(0),\"%2(%3)\",methodArgCount+%4,&methodArgTypes[%4]);")
                              .arg(QString::number(i+1), slot.name, slot.paramsAsString(ASTFunction::Normalized), QString::number(i)) << endl;
     }
 

src/remoteobjects/qremoteobjectabstractitemmodeladapter_p.h
@@ -112,43 +112,35 @@ private:
     QItemSelectionModel *m_selectionModel;
     QVector<int> m_availableRoles;
 };
-namespace{
-    const int s1[] = {qMetaTypeId<IndexList>(), qMetaTypeId<IndexList>(), qMetaTypeId<QVector<int> >()};
-    const int s2[] = {qMetaTypeId<IndexList>(), qMetaTypeId<int>(), qMetaTypeId<int>()};
-    const int s3[] = {qMetaTypeId<IndexList>(), qMetaTypeId<int>(), qMetaTypeId<int>()};
-    const int s4[] = {qMetaTypeId<IndexList>(), qMetaTypeId<int>(), qMetaTypeId<int>(), qMetaTypeId<IndexList>(),qMetaTypeId<int>()};
-    const int s5[] = {qMetaTypeId<IndexList>(), qMetaTypeId<IndexList>()};
-    const int s7[] = {qMetaTypeId<Qt::Orientation>(), qMetaTypeId<int>(), qMetaTypeId<int>()};
-    const int s8[] = {qMetaTypeId<IndexList>(), qMetaTypeId<int>(), qMetaTypeId<int>()};
-    const int * const signalArgTypes[] = {Q_NULLPTR, s1, s2, s3, s4, s5, Q_NULLPTR, s7, s8};
-}
 
 template <class ObjectType, class AdapterType>
 struct QAbstractItemAdapterSourceAPI : public SourceApiMap
 {
     QAbstractItemAdapterSourceAPI(const QString &name)
         : SourceApiMap()
+        , signalArgTypes{}
+        , methodArgTypes{}
         , m_name(name)
     {
         _properties[0] = 2;
         _properties[1] = qtro_prop_index<AdapterType>(&AdapterType::availableRoles, static_cast<QVector<int> (QObject::*)()>(0),"availableRoles");
         _properties[2] = qtro_prop_index<AdapterType>(&AdapterType::roleNames, static_cast<QIntHash (QObject::*)()>(0),"roleNames");
         _signals[0] = 9;
-        _signals[1] = qtro_signal_index<AdapterType>(&AdapterType::availableRolesChanged, static_cast<void (QObject::*)()>(0),signalArgCount+0,signalArgTypes[0]);
-        _signals[2] = qtro_signal_index<AdapterType>(&AdapterType::dataChanged, static_cast<void (QObject::*)(IndexList,IndexList,QVector<int>)>(0),signalArgCount+1,signalArgTypes[1]);
-        _signals[3] = qtro_signal_index<AdapterType>(&AdapterType::rowsInserted, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+2,signalArgTypes[2]);
-        _signals[4] = qtro_signal_index<AdapterType>(&AdapterType::rowsRemoved, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+3,signalArgTypes[3]);
-        _signals[5] = qtro_signal_index<AdapterType>(&AdapterType::rowsMoved, static_cast<void (QObject::*)(IndexList,int,int,IndexList,int)>(0),signalArgCount+4,signalArgTypes[4]);
-        _signals[6] = qtro_signal_index<AdapterType>(&AdapterType::currentChanged, static_cast<void (QObject::*)(IndexList,IndexList)>(0),signalArgCount+5,signalArgTypes[5]);
-        _signals[7] = qtro_signal_index<ObjectType>(&ObjectType::modelReset, static_cast<void (QObject::*)()>(0),signalArgCount+6,signalArgTypes[6]);
-        _signals[8] = qtro_signal_index<ObjectType>(&ObjectType::headerDataChanged, static_cast<void (QObject::*)(Qt::Orientation,int,int)>(0),signalArgCount+7,signalArgTypes[7]);
-        _signals[9] = qtro_signal_index<AdapterType>(&AdapterType::columnsInserted, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+8,signalArgTypes[8]);
+        _signals[1] = qtro_signal_index<AdapterType>(&AdapterType::availableRolesChanged, static_cast<void (QObject::*)()>(0),signalArgCount+0,&signalArgTypes[0]);
+        _signals[2] = qtro_signal_index<AdapterType>(&AdapterType::dataChanged, static_cast<void (QObject::*)(IndexList,IndexList,QVector<int>)>(0),signalArgCount+1,&signalArgTypes[1]);
+        _signals[3] = qtro_signal_index<AdapterType>(&AdapterType::rowsInserted, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+2,&signalArgTypes[2]);
+        _signals[4] = qtro_signal_index<AdapterType>(&AdapterType::rowsRemoved, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+3,&signalArgTypes[3]);
+        _signals[5] = qtro_signal_index<AdapterType>(&AdapterType::rowsMoved, static_cast<void (QObject::*)(IndexList,int,int,IndexList,int)>(0),signalArgCount+4,&signalArgTypes[4]);
+        _signals[6] = qtro_signal_index<AdapterType>(&AdapterType::currentChanged, static_cast<void (QObject::*)(IndexList,IndexList)>(0),signalArgCount+5,&signalArgTypes[5]);
+        _signals[7] = qtro_signal_index<ObjectType>(&ObjectType::modelReset, static_cast<void (QObject::*)()>(0),signalArgCount+6,&signalArgTypes[6]);
+        _signals[8] = qtro_signal_index<ObjectType>(&ObjectType::headerDataChanged, static_cast<void (QObject::*)(Qt::Orientation,int,int)>(0),signalArgCount+7,&signalArgTypes[7]);
+        _signals[9] = qtro_signal_index<AdapterType>(&AdapterType::columnsInserted, static_cast<void (QObject::*)(IndexList,int,int)>(0),signalArgCount+8,&signalArgTypes[8]);
         _methods[0] = 5;
-        _methods[1] = qtro_method_index<AdapterType>(&AdapterType::replicaSizeRequest, static_cast<void (QObject::*)(IndexList)>(0),"replicaSizeRequest(IndexList)",methodArgCount+0,methodArgTypes[0]);
-        _methods[2] = qtro_method_index<AdapterType>(&AdapterType::replicaRowRequest, static_cast<void (QObject::*)(IndexList,IndexList,QVector<int>)>(0),"replicaRowRequest(IndexList,IndexList,QVector<int>)",methodArgCount+1,methodArgTypes[1]);
-        _methods[3] = qtro_method_index<AdapterType>(&AdapterType::replicaHeaderRequest, static_cast<void (QObject::*)(QVector<Qt::Orientation>,QVector<int>,QVector<int>)>(0),"replicaHeaderRequest(QVector<Qt::Orientation>,QVector<int>,QVector<int>)",methodArgCount+2,methodArgTypes[2]);
-        _methods[4] = qtro_method_index<AdapterType>(&AdapterType::replicaSetCurrentIndex, static_cast<void (QObject::*)(IndexList,QItemSelectionModel::SelectionFlags)>(0),"replicaSetCurrentIndex(IndexList,QItemSelectionModel::SelectionFlags)",methodArgCount+3,methodArgTypes[3]);
-        _methods[5] = qtro_method_index<AdapterType>(&AdapterType::replicaSetData, static_cast<void (QObject::*)(IndexList,QVariant,int)>(0),"replicaSetData(IndexList,QVariant,int)",methodArgCount+4,methodArgTypes[4]);
+        _methods[1] = qtro_method_index<AdapterType>(&AdapterType::replicaSizeRequest, static_cast<void (QObject::*)(IndexList)>(0),"replicaSizeRequest(IndexList)",methodArgCount+0,&methodArgTypes[0]);
+        _methods[2] = qtro_method_index<AdapterType>(&AdapterType::replicaRowRequest, static_cast<void (QObject::*)(IndexList,IndexList,QVector<int>)>(0),"replicaRowRequest(IndexList,IndexList,QVector<int>)",methodArgCount+1,&methodArgTypes[1]);
+        _methods[3] = qtro_method_index<AdapterType>(&AdapterType::replicaHeaderRequest, static_cast<void (QObject::*)(QVector<Qt::Orientation>,QVector<int>,QVector<int>)>(0),"replicaHeaderRequest(QVector<Qt::Orientation>,QVector<int>,QVector<int>)",methodArgCount+2,&methodArgTypes[2]);
+        _methods[4] = qtro_method_index<AdapterType>(&AdapterType::replicaSetCurrentIndex, static_cast<void (QObject::*)(IndexList,QItemSelectionModel::SelectionFlags)>(0),"replicaSetCurrentIndex(IndexList,QItemSelectionModel::SelectionFlags)",methodArgCount+3,&methodArgTypes[3]);
+        _methods[5] = qtro_method_index<AdapterType>(&AdapterType::replicaSetData, static_cast<void (QObject::*)(IndexList,QVariant,int)>(0),"replicaSetData(IndexList,QVariant,int)",methodArgCount+4,&methodArgTypes[4]);
     }
 
     QString name() const Q_DECL_OVERRIDE { return m_name; }
@@ -275,6 +267,7 @@ struct QAbstractItemAdapterSourceAPI : public SourceApiMap
     int _signals[10];
     int _methods[6];
     int signalArgCount[9];
+    const int* signalArgTypes[9];
     int methodArgCount[5];
     const int* methodArgTypes[5];
     QString m_name;

src/remoteobjects/qremoteobjectsource.h
@@ -67,9 +67,8 @@ static inline int qtro_prop_index(Func1, Func2, const char *propName)
 }
 
 template <class ObjectType, typename Func1, typename Func2>
-static inline int qtro_signal_index(Func1 func, Func2, int *count, const int *types)
+static inline int qtro_signal_index(Func1 func, Func2, int *count, int const **types)
 {
-    Q_UNUSED(types);
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
     reinterpret_cast<typename Type1::Object *>(0)->qt_check_for_QOBJECT_macro(*reinterpret_cast<typename Type1::Object *>(0));
@@ -83,7 +82,7 @@ static inline int qtro_signal_index(Func1 func, Func2, int *count, const int *ty
                       "Return types are not compatible.");
     const QMetaMethod sig = QMetaMethod::fromSignal(func);
     *count = Type2::ArgumentCount;
-    types = QtPrivate::ConnectionTypes<typename Type2::Arguments>::types();
+    *types = QtPrivate::ConnectionTypes<typename Type2::Arguments>::types();
     return sig.methodIndex();
 }
 
@@ -104,9 +103,8 @@ static inline void qtro_method_test(Func1, Func2)
 }
 
 template <class ObjectType, typename Func1, typename Func2>
-static inline int qtro_method_index(Func1, Func2, const char *methodName, int *count, const int *types)
+static inline int qtro_method_index(Func1, Func2, const char *methodName, int *count, int const **types)
 {
-    Q_UNUSED(types);
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
     reinterpret_cast<typename Type1::Object *>(0)->qt_check_for_QOBJECT_macro(*reinterpret_cast<typename Type1::Object *>(0));
@@ -119,7 +117,7 @@ static inline int qtro_method_index(Func1, Func2, const char *methodName, int *c
     Q_STATIC_ASSERT_X((QtPrivate::AreArgumentsCompatible<typename Type1::ReturnType, typename Type2::ReturnType>::value),
                       "Return types are not compatible.");
     *count = Type2::ArgumentCount;
-    types = QtPrivate::ConnectionTypes<typename Type2::Arguments>::types();
+    *types = QtPrivate::ConnectionTypes<typename Type2::Arguments>::types();
     return ObjectType::staticMetaObject.indexOfMethod(methodName);
 }
 

commit f2bb6cf6b95c6a5b277dc8af333354cd35fd42f6

repc: Fix generated code for class with one SIGNAL
With this .rep file:
class MyClass
{
    SIGNAL(foo())
}

We got the following error:
.moc/../rep_classwithsignalonlytest_merged.h:153:11: error: class
‘MyClassSourceAPI<ObjectType>’ does not have any field named
‘methodArgTypes’
         , methodArgTypes{}

Fix this by not generating the code for the member initializers at all. The
members are initialized within the ctor anyway

Regression introduced by fc881f28987764e48237d648622e6227c2ad3237

Change-Id: I8141aa799e02408a6484a13a28064813a2dc5007
Reviewed-by: Continuous Integration (KDAB) <build@kdab.com>
Reviewed-by: Brett Stottlemyer <bstottle@ford.com>

tools/repc/repcodegenerator.cpp
@@ -811,8 +811,6 @@ void RepCodeGenerator::generateSourceAPI(QTextStream &out, const ASTClass &astCl
     }
     out << QString::fromLatin1("    %1()").arg(className) << endl;
     out << QStringLiteral("        : SourceApiMap()") << endl;
-    out << QStringLiteral("        , signalArgTypes{}") << endl;
-    out << QStringLiteral("        , methodArgTypes{}") << endl;
     out << QStringLiteral("    {") << endl;
     const int propCount = astClass.properties.count();
     out << QString::fromLatin1("        _properties[0] = %1;").arg(propCount) << endl;

tests/auto/repcodegenerator/classwithsignalonlytest.rep
@@ -1,0 +1,4 @@
+class MyClass
+{
+    SIGNAL(mySignal(int))
+}

tests/auto/repcodegenerator/repcodegenerator.pro
@@ -5,8 +5,11 @@ QT -= gui
 
 SOURCES += tst_repcodegenerator.cpp
 
-REP_FILES = preprocessortest.rep
+REP_FILES += \
+    classwithsignalonlytest.rep \
+    preprocessortest.rep \
 
-REPC_REPLICA = $$REP_FILES
+# Make sure we test both source + replica generated code
+REPC_MERGED = $$REP_FILES
 
 contains(QT_CONFIG, c++11): CONFIG += c++11

tests/auto/repcodegenerator/tst_repcodegenerator.cpp
@@ -39,7 +39,7 @@
 **
 ****************************************************************************/
 
-#include "rep_preprocessortest_replica.h"
+#include "rep_preprocessortest_merged.h"
 
 #include <QTest>
 