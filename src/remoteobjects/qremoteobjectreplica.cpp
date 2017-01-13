bf424ef29d7089e130b754206441da6f26760812
Brett Stottlemyer <bstottle@ford.com>
2017-01-13 07:24:30 -0500

    Fix build on WinRT
    It doesn't seem to like concatenating strings in a static assert.
    
    Change-Id: I541ec7dad362f526d1f95ef21afddcc71c7aa2b1
    Reviewed-by: Continuous Integration (KDAB) <build@kdab.com>
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/remoteobjects/qremoteobjectreplica.cpp
+++ src/remoteobjects/qremoteobjectreplica.cpp
@@ -64,8 +64,10 @@ QT_BEGIN_NAMESPACE
 using namespace QRemoteObjectPackets;
 
 Q_STATIC_ASSERT_X(&QRemoteObjectReplica::staticMetaObject == &QRemoteObjectDynamicReplica::staticMetaObject,
-                  "m_signalOffset initializer expects QRemoteObjectDynamicReplica to not have a unique staticMetaObject "
-                  "(see FIX #1, #2, #3 in commented code.)");
+                  "m_signalOffset initializer expects QRemoteObjectDynamicReplica to not have a unique staticMetaObject");
+// If QRemoteObjectDynamicReplica ever gets its own staticMetaObject, some commented out code will need to be
+// used.  It was changed to avoid a Coverity complaint.  We use the above static assert to detect if this changes
+// in the future.  See FIX #1, #2, #3 in this file.
 
 QRemoteObjectReplicaPrivate::QRemoteObjectReplicaPrivate(const QString &name, const QMetaObject *meta, QRemoteObjectNode *_node)
     : QObject(Q_NULLPTR), m_objectName(name), m_metaObject(meta), m_numSignals(0), m_methodOffset(0)
