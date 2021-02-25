2309c0815824757f47e5389136a8030f60ffe07b
Brett Stottlemyer <bstottle@ford.com>
2021-02-25 07:52:16 -0500

    Attempt to fix flaky test
    By increasing the timeout
    
    Task-number: QTBUG-82284
    Pick-to: 5.15 dev
    Change-Id: Ied4c9371548732ce714437a456ca90a441d1c944
    Reviewed-by: Sona Kurazyan <sona.kurazyan@qt.io>

--- tests/auto/modelview/tst_modelview.cpp
+++ tests/auto/modelview/tst_modelview.cpp
@@ -604,6 +604,12 @@ void TestModelView::setup_models(QRemoteObjectHost &basicServer, QRemoteObjectNo
     port += 2;
 }
 
+#ifdef SLOW_MODELTEST
+#define MODELTEST_WAIT_TIME 25000
+#else
+#define MODELTEST_WAIT_TIME
+#endif
+
 void TestModelView::testEmptyModel()
 {
     _SETUP_TEST_
@@ -616,7 +622,7 @@ void TestModelView::testEmptyModel()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     compareData(&emptyModel, model.data());
 }
@@ -628,7 +634,7 @@ void TestModelView::testInitialData()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     compareData(&m_sourceModel, model.data());
 }
@@ -640,7 +646,7 @@ void TestModelView::testInitialDataTree()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     compareTreeData(&m_sourceModel, model.data());
 }
@@ -652,7 +658,7 @@ void TestModelView::testHeaderData()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     // ask for all Data members first, so we don't have to wait for update signals
     QSignalSpy spyHeader(model.data(), SIGNAL(headerDataChanged(Qt::Orientation,int,int)));
@@ -675,7 +681,7 @@ void TestModelView::testDataChangedTree()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     compareTreeData(&m_sourceModel, model.data());
     QSignalSpy dataChangedSpy(model.data(), SIGNAL(dataChanged(QModelIndex,QModelIndex,QVector<int>)));
@@ -723,7 +729,7 @@ void TestModelView::testFlags()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     QSignalSpy dataChangedSpy(model.data(), SIGNAL(dataChanged(QModelIndex,QModelIndex,QVector<int>)));
     for (int i = 10; i < 20; ++i) {
@@ -749,7 +755,7 @@ void TestModelView::testDataChanged()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     QSignalSpy dataChangedSpy(model.data(), SIGNAL(dataChanged(QModelIndex,QModelIndex,QVector<int>)));
     for (int i = 10; i < 20; ++i)
@@ -772,7 +778,7 @@ void TestModelView::testDataInsertion()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     QVector<QModelIndex> pending;
 
@@ -836,7 +842,7 @@ void TestModelView::testDataInsertionTree()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     const QVector<int> roles = model->availableRoles();
 
@@ -925,7 +931,7 @@ void TestModelView::testDataRemoval()
     model->setRootCacheSize(1000);
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     QVector<InsertedRow> removedRows;
     QSignalSpy rowSpy(model.data(), SIGNAL(rowsRemoved(QModelIndex,int,int)));
@@ -966,7 +972,7 @@ void TestModelView::testRoleNames()
     repModel->setRootCacheSize(1500);
     FetchData f(repModel.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     // test custom role names
     QCOMPARE(repModel.data()->roleNames(), m_listModel.roleNames());
@@ -1008,16 +1014,10 @@ void TestModelView::testServerInsertDataTree()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     compareData(&testTreeModel, model.data());
 }
-
-#ifdef SLOW_MODELTEST
-#define MODELTEST_WAIT_TIME 25000
-#else
-#define MODELTEST_WAIT_TIME
-#endif
 
 void TestModelView::testModelTest()
 {
@@ -1038,7 +1038,7 @@ void TestModelView::testSortFilterModel()
 
     FetchData f(repModel.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     QSortFilterProxyModel clientSort;
     clientSort.setSourceModel(repModel.data());
@@ -1057,7 +1057,7 @@ void TestModelView::testSetData()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
     compareTreeData(&m_sourceModel, model.data(), model->availableRoles());
 
     //fetched and verified initial state, now setData on the client
@@ -1088,7 +1088,7 @@ void TestModelView::testSetDataTree()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
     compareTreeData(&m_sourceModel, model.data(), model->availableRoles());
 
     //fetched and verified initial state, now setData on the client
@@ -1143,7 +1143,7 @@ void TestModelView::testSelectionFromReplica()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     replicaSelectionModel->setCurrentIndex(model->index(1,0), QItemSelectionModel::ClearAndSelect | QItemSelectionModel::Current);
     QTRY_COMPARE(selectionModel.currentIndex().row(), 1);
@@ -1164,7 +1164,7 @@ void TestModelView::testSelectionFromSource()
 
     FetchData f(model.data());
     f.addAll();
-    QVERIFY(f.fetchAndWait());
+    QVERIFY(f.fetchAndWait(MODELTEST_WAIT_TIME));
 
     selectionModel.setCurrentIndex(simpleModel.index(1,0), QItemSelectionModel::ClearAndSelect | QItemSelectionModel::Current);
     QTRY_COMPARE(replicaSelectionModel->currentIndex().row(), 1);
