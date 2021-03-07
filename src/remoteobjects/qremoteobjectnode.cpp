e910e445a4860467bc99e2bb446978fa790815dd
Brett Stottlemyer <bstottle@ford.com>
2021-03-07 08:03:33 -0500

    ExternalIODevice fixes
    Make sure the input QIODevice is valid and the disconnect signal (if
    available) is connected.
    
    Task-number: QTBUG-84640
    Pick-to: 5.15 5.12
    Change-Id: I9e65dc903ce0e92da4f4a1e07fd5d6b66815e2a4
    Reviewed-by: Sona Kurazyan <sona.kurazyan@qt.io>

--- src/remoteobjects/qremoteobjectnode.cpp
+++ src/remoteobjects/qremoteobjectnode.cpp
@@ -2289,6 +2289,10 @@ bool QRemoteObjectNode::connectToNode(const QUrl &address)
 void QRemoteObjectNode::addClientSideConnection(QIODevice *ioDevice)
 {
     Q_D(QRemoteObjectNode);
+    if (!ioDevice || !ioDevice->isOpen()) {
+        qWarning() << "A null or closed QIODevice was passed to addClientSideConnection().  Ignoring.";
+        return;
+    }
     ExternalIoDevice *device = new ExternalIoDevice(ioDevice, this);
     connect(device, &IoDeviceBase::readyRead, this, [d, device]() {
         d->onClientRead(device);
@@ -2569,6 +2573,10 @@ bool QRemoteObjectHostBase::disableRemoting(QObject *remoteObject)
 void QRemoteObjectHostBase::addHostSideConnection(QIODevice *ioDevice)
 {
     Q_D(QRemoteObjectHostBase);
+    if (!ioDevice || !ioDevice->isOpen()) {
+        qWarning() << "A null or closed QIODevice was passed to addHostSideConnection().  Ignoring.";
+        return;
+    }
     if (!d->remoteObjectIo)
         d->remoteObjectIo = new QRemoteObjectSourceIo(this);
     ExternalIoDevice *device = new ExternalIoDevice(ioDevice, this);
