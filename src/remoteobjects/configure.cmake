959de3280b197920d27b4720d507a81d12383488
Sona Kurazyan <sona.kurazyan@qt.io>
2021-04-15 10:57:56 +0200

    Add a configurable Qt feature for enabling ham library on QNX
    Change-Id: Ief4ed6121bd44e480d5c7760286d3a498aa61163
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Brett Stottlemyer <bstottle@ford.com>

---
+++ src/remoteobjects/configure.cmake
@@ -0,0 +1,26 @@
+
+
+#### Inputs
+
+
+
+#### Libraries
+
+
+
+#### Tests
+
+
+
+#### Features
+
+qt_feature("use_ham" PUBLIC
+    LABEL "High Availability Manager (ham)"
+    PURPOSE "Use QNX's High Availability Manager (ham) library"
+    AUTODETECT OFF
+    CONDITION QNX
+)
+qt_feature_definition("use_ham" "QT_NO_USE_HAM" NEGATE VALUE "1")
+qt_configure_add_summary_section(NAME "Qt Remote Objects")
+qt_configure_add_summary_entry(ARGS "use_ham")
+qt_configure_end_summary_section() # end of "Qt Remote Objects" section
