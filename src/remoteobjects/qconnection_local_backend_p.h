b2cec690a7f0424b96cf09c6192f130ed17a0f1d
Øystein Heskestad <oystein.heskestad@qt.io>
2025-06-12 14:57:02 +0200

    Mark QtRemoteObjects as security critical
    The QtRemoteObjects serialization is built on top of QDataStream, but
    QDataStream is not secure. Mark all files parsing data as security
    critical.
    
    The security is dependent on the backend used. The local and QNX
    backends communicate only locally but are insecure on a compromised
    host. The TCP backend is by default insecure, but it supports TLS,
    and can be secure if that is used.
    
    The two files in the repparser directory are used for parsing, but
    only at build time, and are therefore kept at security significant.
    
    The remaining files are marked as security signifcant.
    
    QUIP: 23
    Task-number: QTBUG-135570
    Pick-to: 6.8
    Change-Id: I11176c036d95f8c706bd05e1cab1ba499003f683
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit a7ca9d59dd4c02a26573f54e0597d94c8e6a475d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit df74b85be092493dde828172fea79f9fcd1e67b4)

--- src/remoteobjects/qconnection_local_backend_p.h
+++ src/remoteobjects/qconnection_local_backend_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2017-2015 Ford Motor Company
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:declarations-only
 
 #ifndef QCONNECTIONCLIENTFACTORY_P_H
 #define QCONNECTIONCLIENTFACTORY_P_H
