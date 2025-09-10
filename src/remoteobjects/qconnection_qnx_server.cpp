e6d6b49afad6a05708dff9171646567187c103eb
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-09-10 15:52:16 +0300

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit af358744e19bf4d659ea086a3792919692c34b3a.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial SPDX license identifiers.
    
    Task-number: QTLTSOSS-264
    Change-Id: Ieaa2971c36b2654606e54b5a30ab6ec8e2ba4495
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/remoteobjects/qconnection_qnx_server.cpp
+++ src/remoteobjects/qconnection_qnx_server.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2017-2016 Ford Motor Company
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qconnection_qnx_global_p.h"
 #include "qconnection_qnx_qiodevices_p.h"
--- src/remoteobjects/qconnection_tcpip_backend_p.h
+++ src/remoteobjects/qconnection_tcpip_backend_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2017-2015 Ford Motor Company
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QCONNECTIONTCPIPBACKEND_P_H
 #define QCONNECTIONTCPIPBACKEND_P_H
