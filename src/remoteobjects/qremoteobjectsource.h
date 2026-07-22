06666fd8313f190631a8cfe8533c40a4dca9ce58
Marc Mutz <marc.mutz@qt.io>
2026-07-22 11:48:32 +0200

    Remove "static inline" from qtro_ functions
    Functions in headers shouldn't be marked as static, because that
    prevents deduplication at link-time. As function templates, they also
    don't need to be marked as inline, as that's the default linkage for
    function templates, anyway.
    
    Amends 7cc03eac6fb5aa7bcb91201f5ce3251049ad0bbb (5.9.8) and
    c96a4193e5d4d46804e8a11fad3b0e95d02b037b (5.12.4).
    
    Pick-to: 6.8
    Change-Id: I42c7d5fe8e0de928a03c611eb441b30347351d35
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit f87bd460a1995155be7dd3ca219a95888b21e2f4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit cf93ce9cf1e60fca2b9497b24ec9164f3941aafc)

--- src/remoteobjects/qremoteobjectsource.h
+++ src/remoteobjects/qremoteobjectsource.h
@@ -15,7 +15,7 @@ namespace QtPrivate {
 
 //Based on compile time checks for static connect() from qobjectdefs_impl.h
 template <class ObjectType, typename Func1, typename Func2>
-static inline int qtro_property_index(Func1, Func2, const char *propName)
+int qtro_property_index(Func1, Func2, const char *propName)
 {
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
@@ -31,7 +31,7 @@ static inline int qtro_property_index(Func1, Func2, const char *propName)
 }
 
 template <class ObjectType, typename Func1, typename Func2>
-static inline int qtro_signal_index(Func1 func, Func2, int *count, int const **types)
+int qtro_signal_index(Func1 func, Func2, int *count, int const **types)
 {
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
@@ -50,7 +50,7 @@ static inline int qtro_signal_index(Func1 func, Func2, int *count, int const **t
 }
 
 template <class ObjectType, typename Func1, typename Func2>
-static inline void qtro_method_test(Func1, Func2)
+void qtro_method_test(Func1, Func2)
 {
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
@@ -69,7 +69,7 @@ int qtro_method_index_impl(const QMetaObject *staticMetaObj, const char *classNa
                            const char *methodName, int *count, const int **types);
 
 template <class ObjectType, typename Func1, typename Func2>
-static inline int qtro_method_index(Func1, Func2, const char *methodName, int *count, int const **types)
+int qtro_method_index(Func1, Func2, const char *methodName, int *count, int const **types)
 {
     typedef QtPrivate::FunctionPointer<Func1> Type1;
     typedef QtPrivate::FunctionPointer<Func2> Type2;
@@ -90,7 +90,7 @@ static inline int qtro_method_index(Func1, Func2, const char *methodName, int *c
 }
 
 template <class ObjectType>
-static inline QByteArray qtro_enum_signature(const char *enumName)
+QByteArray qtro_enum_signature(const char *enumName)
 {
     const auto qme = ObjectType::staticMetaObject.enumerator(ObjectType::staticMetaObject.indexOfEnumerator(enumName));
     return QByteArrayLiteral("1::2").replace("1", qme.scope()).replace("2", qme.name());
