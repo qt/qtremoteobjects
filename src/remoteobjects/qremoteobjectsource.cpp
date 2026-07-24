b7f0da83a1cff48ca3421e404e1d4cc9836bae3c
Ivan Solovev <ivan.solovev@qt.io>
2026-07-24 13:36:16 +0200

    qremoteobjectsource.cpp: be more explicit on what we delete
    In the ranged-for loops over m_children, each 'it' variable is
    a QPointer. Calling delete on it is perfectly fine, because it goes
    via operator T*().
    However, the follow-up change will take 'it' by const-ref instead of
    by-value, and then the code will be a bit confusing (i.e. calling
    delete on a const ref).
    
    To make it more readable, explicitly call data(), to signal that
    we're deleting the underlying data.
    
    Amends d9ee9fff1b9291395ba7ab75d7e46ddf4a9ff3a4.
    
    Pick-to: 6.11 6.8
    Change-Id: I2a9f7d8eff55a7101d7b067a6454e3517b4cc013
    Reviewed-by: Tatiana Borisova <tatiana.borisova@qt.io>
    (cherry picked from commit 5ae43a67a5a6ebc5e7fd33dea0a1870c8692ad8a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/remoteobjects/qremoteobjectsource.cpp
+++ src/remoteobjects/qremoteobjectsource.cpp
@@ -346,7 +346,7 @@ QRemoteObjectSource::~QRemoteObjectSource()
         // We used QPointers for m_children because we don't control the lifetime of child QObjects
         // Since the this/source QObject's parent is the referenced QObject, it could have already
         // been deleted
-        delete it;
+        delete it.data();
     }
 }
 
@@ -356,7 +356,7 @@ QRemoteObjectRootSource::~QRemoteObjectRootSource()
         // We used QPointers for m_children because we don't control the lifetime of child QObjects
         // Since the this/source QObject's parent is the referenced QObject, it could have already
         // been deleted
-        delete it;
+        delete it.data();
     }
     d->m_sourceIo->unregisterSource(this);
     // removeListener tries to modify d->m_listeners, this is O(N²),
